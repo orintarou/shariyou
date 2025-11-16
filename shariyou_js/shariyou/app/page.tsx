611ef4eb955a69f569de36a56be6d204a9d8223b
Kuma <kchan440@gmail.com>
2025-11-16 10:06:08 -0500

    initial nextjs setup

---
+++ shariyou_js/shariyou/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from 'react';
+import {Component} from 'react-dom';
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="font-serif flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+          Describe your Art.
+        </div>
+        <form>
+          <textarea className="text-s w-[50vw] mt-[-50vh]" placeholder="Hi!  I'm an aspiring hip hop artist from NYC.  My music is heavily practiced on beats that are sad with lofi elements.
+          I have about 10 songs ready to release."></textarea>
+          <input onClick={handleClick} className="font-sans text-xs w-[100px] h-[20px] m-[20px] float-right border-[.2px] hover:bg-[tomato] opacity-[50%]" type="submit"/>
+        </form>
+        <div>
+          - shariyou
+        </div>
+      </main>
+    </div>
+  );
+}
